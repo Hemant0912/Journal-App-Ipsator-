f4cb7fb268bef541a59a22f345a8acc47ab8927b
Hemant Vishwakarma <9450871187hemant@gmail.com>
2025-08-28 17:40:49 +0530

    Added validation, global exception handler, and updated configs

--- src/main/java/net/hemjournalApp/controller/PublicController.java
+++ src/main/java/net/hemjournalApp/controller/PublicController.java
@@ -1,9 +1,13 @@
 package net.hemjournalApp.controller;
-
 import net.hemjournalApp.entity.UserEntity;
 import net.hemjournalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/public")
@@ -17,7 +21,15 @@ public class PublicController {
         return "Ok";
     }
     @PostMapping("create-user")
-    public void createUser(@RequestBody UserEntity userEntity) {
+    public void createUser(@Valid @RequestBody UserEntity userEntity) {
         userService.saveNewUser(userEntity);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
 }
+
--- src/main/java/net/hemjournalApp/entity/UserEntity.java
+++ src/main/java/net/hemjournalApp/entity/UserEntity.java
@@ -6,6 +6,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +18,14 @@ public class UserEntity {
     @Id
     private ObjectId id;
 // it will be not created automatically i.e indexing we have to give it in app.prop
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min=3, message = "Username must be at least 3 charcters long")
     @Indexed(unique = true)
     @NonNull
     private String userName;
     @NonNull
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min=3, message = "Password must be at least 3 charcters long")
     private String password;
     @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
